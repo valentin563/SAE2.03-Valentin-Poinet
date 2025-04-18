bae653550e8ea57089505f2ea00d6a45fa68a0b6
Valentin <valentin.poinet@etu.unilim.fr>
2025-04-18 15:46:25 +0200

    fin

---
+++ DocumentationBD/README.md
@@ -0,0 +1,13 @@
+## Profil :
+
+    Name varchar(255) car le nom du profil peut contenir des chiffres ou des caractères spéciaux.
+
+    Image varchar(255) car le nom de l’image peut contenir des chiffres ou des caractères spéciaux.
+
+    Age INT car l’âge et un nombre entier.
+
+![alt text](image.png)
+
+    Pour les liens entre category et movie, j'ai mis 0,n car une category peut avoir au minimum 0 film et au maximum n film
+
+    Pour les liens entre movie et category, j'ai mis 1,1 car un film peut   avoir au minimum 1 category et au maximum 1 category
---
+++ DocumentationBD/poinet2.sql
@@ -0,0 +1,160 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost
+-- Généré le : ven. 18 avr. 2025 à 13:13
+-- Version du serveur : 10.11.11-MariaDB-0+deb12u1
+-- Version de PHP : 8.3.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `poinet2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Category`
+--
+
+CREATE TABLE `Category` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+--
+-- Déchargement des données de la table `Category`
+--
+
+INSERT INTO `Category` (`id`, `name`) VALUES
+(1, 'Action'),
+(2, 'Comédie'),
+(3, 'Drame'),
+(4, 'Science-fiction'),
+(5, 'Animation'),
+(6, 'Thriller'),
+(7, 'Horreur'),
+(8, 'Aventure'),
+(9, 'Fantaisie'),
+(10, 'Documentaire');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Movie`
+--
+
+CREATE TABLE `Movie` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `year` int(11) DEFAULT NULL,
+  `length` int(11) DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `director` varchar(255) DEFAULT NULL,
+  `id_category` int(11) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `trailer` varchar(255) DEFAULT NULL,
+  `min_age` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+--
+-- Déchargement des données de la table `Movie`
+--
+
+INSERT INTO `Movie` (`id`, `name`, `year`, `length`, `description`, `director`, `id_category`, `image`, `trailer`, `min_age`) VALUES
+(7, 'Interstellar', 2014, 169, 'Un groupe d\'explorateurs voyage à travers un trou de ver pour sauver l\'humanité.', 'Christopher Nolan', 4, 'interstellar.jpg', 'https://www.youtube.com/embed/VaOijhK3CRU?si=76Ke4uw4LYjuLuQ6', 12),
+(12, 'La Liste de Schindler', 1993, 195, 'Un industriel allemand sauve des milliers de Juifs pendant l\'Holocauste.', 'Steven Spielberg', 3, 'schindler.webp', 'https://www.youtube.com/embed/ONWtyxzl-GE?si=xC3ASGGPy5Ib-aPn', 16),
+(17, 'Your Name', 2016, 107, 'Deux adolescents échangent leurs corps de manière mystérieuse.', 'Makoto Shinkai', 5, 'your_name.jpg', 'https://www.youtube.com/embed/AROOK45LXXg?si=aUQyGk2VMCb_ToUL', 10),
+(27, 'Le Bon, la Brute et le Truand', 1966, 161, 'Trois hommes se lancent à la recherche d\'un trésor caché.', 'Sergio Leone', 8, 'bon_brute_truand.jpg', 'https://www.youtube.com/embed/WA1hCZFOPqs?si=TwNZAoM4oj4KpGja', 12);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Profil`
+--
+
+CREATE TABLE `Profil` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `age` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `Profil`
+--
+
+INSERT INTO `Profil` (`id`, `name`, `image`, `age`) VALUES
+(1, 'test', 'test', 5),
+(2, 'test2', 'test2', 254),
+(3, 'test3', 'test3', 12);
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `Category`
+--
+ALTER TABLE `Category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `Movie`
+--
+ALTER TABLE `Movie`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_category` (`id_category`);
+
+--
+-- Index pour la table `Profil`
+--
+ALTER TABLE `Profil`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `Category`
+--
+ALTER TABLE `Category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT pour la table `Movie`
+--
+ALTER TABLE `Movie`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+
+--
+-- AUTO_INCREMENT pour la table `Profil`
+--
+ALTER TABLE `Profil`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `Movie`
+--
+ALTER TABLE `Movie`
+  ADD CONSTRAINT `movie_ibfk_1` FOREIGN KEY (`id_category`) REFERENCES `Category` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
